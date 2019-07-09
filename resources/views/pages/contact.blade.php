64d8173fdbd841f5992569417469c0c5706ad1f5
ping <ph49@njit.edu>
2019-07-09 17:51:05 -0400

    Task: Add a button in contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -7,37 +7,19 @@
 
 
     <h1>This is my contact page</h1>
-    <p class="lead">Please use this form to contact hte sit owner.</p>
+    <p class="lead">Please use this form to contact the site owner.</p>
 
     <form>
         <div class="form-group">
-            <label for="exampleFormControlInput1">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            <label for="email">Email address</label>
+            <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
         </div>
+
         <div class="form-group">
-            <label for="exampleFormControlSelect1">Example select</label>
-            <select class="form-control" id="exampleFormControlSelect1">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-            </select>
+            <label for="body">Message</label>
+            <textarea name="body" class="form-control" id="body" rows="3"></textarea>
         </div>
-        <div class="form-group">
-            <label for="exampleFormControlSelect2">Example multiple select</label>
-            <select multiple class="form-control" id="exampleFormControlSelect2">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlTextarea1">Example textarea</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
+        <button type="submit" class="btn btn-primary mb-2">Submit</button>
     </form>
 
 @endsection
